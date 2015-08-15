cdbed2f3f20d8111f900fcb255e553135eedb120
Satoshi Watanabe <sassy.watanabe@gmail.com>
2015-08-16 01:25:11 +0900

    add some test.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # KeiseiBusTime
 
 [![Join the chat at https://gitter.im/sassy/KeiseiBusTime](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/sassy/KeiseiBusTime?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
 You want to search the starting time of Keisei bus (from Fujitsu to Makuhari-Hongoh)
 
 # Install
--- main_test.go
+++ main_test.go
@@ -18,7 +18,69 @@ func TestDepartureTime(t *testing.T) {
 }
 
 func TestDepartureTime2(t *testing.T) {
+	hour, minutes := departureTime("0:0")
+	expected_hour := 0
+	expected_minutes := 0
+	if hour != expected_hour {
+		t.Errorf("got %v\nwant %v", hour, expected_hour)
+	}
+	if minutes != expected_minutes {
+		t.Errorf("got %v\nwant %v", minutes, expected_minutes)
+	}
+}
+
+func TestDepartureTime3(t *testing.T) {
+	hour, minutes := departureTime("00:00")
+	expected_hour := 0
+	expected_minutes := 0
+	if hour != expected_hour {
+		t.Errorf("got %v\nwant %v", hour, expected_hour)
+	}
+	if minutes != expected_minutes {
+		t.Errorf("got %v\nwant %v", minutes, expected_minutes)
+	}
+}
+
+func TestDepartureTime4(t *testing.T) {
+	hour, minutes := departureTime("23:59")
+	expected_hour := 23
+	expected_minutes := 59
+	if hour != expected_hour {
+		t.Errorf("got %v\nwant %v", hour, expected_hour)
+	}
+	if minutes != expected_minutes {
+		t.Errorf("got %v\nwant %v", minutes, expected_minutes)
+	}
+}
+
+func TestDepartureTime5(t *testing.T) {
 	hour, minutes := departureTime("44:30")
+	now := time.Now()
+	expected_hour := now.Hour()
+	expected_minutes := now.Minute()
+	if hour != expected_hour {
+		t.Errorf("got %v\nwant %v", hour, expected_hour)
+	}
+	if minutes != expected_minutes {
+		t.Errorf("got %v\nwant %v", minutes, expected_minutes)
+	}
+}
+
+func TestDepartureTime6(t *testing.T) {
+	hour, minutes := departureTime("24:30")
+	now := time.Now()
+	expected_hour := now.Hour()
+	expected_minutes := now.Minute()
+	if hour != expected_hour {
+		t.Errorf("got %v\nwant %v", hour, expected_hour)
+	}
+	if minutes != expected_minutes {
+		t.Errorf("got %v\nwant %v", minutes, expected_minutes)
+	}
+}
+
+func TestDepartureTime7(t *testing.T) {
+	hour, minutes := departureTime("12:60")
 	now := time.Now()
 	expected_hour := now.Hour()
 	expected_minutes := now.Minute()
